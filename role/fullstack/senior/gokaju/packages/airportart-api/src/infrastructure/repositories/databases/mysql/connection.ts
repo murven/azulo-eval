73e117bc3dade6bd27aef81238656127207ae0c1
gokaju <gokaju23@gmail.com>
2022-09-18 21:46:05 -0500

    Docker and docker-compose configuration is added

--- role/fullstack/senior/gokaju/packages/airportart-api/src/infrastructure/repositories/databases/mysql/connection.ts
+++ role/fullstack/senior/gokaju/packages/airportart-api/src/infrastructure/repositories/databases/mysql/connection.ts
@@ -1,10 +1,12 @@
 import { createPool } from "mysql2";
 
+import { constants } from "../../../../config";
+
 export const connection = createPool({
-  host: "localhost",
-  user: "root",
-  password: "rootP@ssw0rd",
-  database: "airport",
+  host: constants.DB_HOST,
+  user: constants.DB_USER,
+  password: constants.DB_PASSWORD,
+  database: constants.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
